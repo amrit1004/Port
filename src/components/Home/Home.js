4c98d687389fec97d4d2632373327ab46b76c02b
Amrit <amritsinghal2211@gmail.com>
2025-07-04 22:43:56 +0530

    made changes to responsivness

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -20,49 +20,51 @@ function Home() {
       <Container fluid className="home-section" id="home" ref={homeRef}>
         <Particle />
         <Container className="home-content">
-          <Row>
-            <Col md={7} className="home-header">
+          <Row className="align-items-center">
+            <Col lg={7} md={12} className="home-header order-2 order-lg-1">
               <div className="section-animate" ref={contentRef}>
-                <h1 style={{ paddingBottom: 15 }} className="heading">
+                <h1 style={{ paddingBottom: 15 }} className="heading responsive-heading">
                   Hi There!{" "}
                   <span className="wave" role="img" aria-labelledby="wave">
                     👋🏻
                   </span>
                 </h1>
 
-                <h1 className="heading-name">
+                <h1 className="heading-name responsive-heading">
                   I'M
                   <strong className="main-name"> AMRIT SINGHAL</strong>
                 </h1>
 
-                <div style={{ padding: 50, textAlign: "left" }}>
+                <div className="type-container" style={{ padding: "30px 0", textAlign: "left" }}>
                   <Type />
                 </div>
               </div>
             </Col>
 
-            <Col md={5} style={{ paddingBottom: 20 }}>
-              <img
-                src={homeLogo}
-                alt="home pic"
-                className="img-fluid"
-                style={{ maxHeight: "450px" }}
-              />
+            <Col lg={5} md={12} className="order-1 order-lg-2 mb-4 mb-lg-0">
+              <div className="home-img-container">
+                <img
+                  src={homeLogo}
+                  alt="home pic"
+                  className="img-fluid"
+                  style={{ maxHeight: "450px", width: "100%", objectFit: "contain" }}
+                />
+              </div>
             </Col>
           </Row>
         </Container>
       </Container>
       <Home2 />
-      <div id="about">
+      <div id="about" className="section-padding">
         <About />
       </div>
-      <div id="experience">
+      <div id="experience" className="section-padding">
         <Experience />
       </div>
-      <div id="project">
+      <div id="project" className="section-padding">
         <Projects />
       </div>
-      <div id="contact">
+      <div id="contact" className="section-padding">
         <Contact />
       </div>
       <Footer />
